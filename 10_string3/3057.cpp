24f17c20c11f0e98339fd84ad0fd8f6c28bc52fe
yukariko <yukariko@github.com>
2021-12-15 23:43:24 +0900

    update the 3057 solution correctly

--- 10_string3/3057.cpp
+++ 10_string3/3057.cpp
@@ -8,13 +8,14 @@ using ld = long double;
 
 using namespace std;
 
+const int BASE = 7;
 const int MOD = 1e9 + 7;
 
-template <int BASE = 29>
 struct Hash
 {
 	vector<int> h, p;
-	Hash (const string& s) : h(s.size()), p(s.size())
+
+	Hash(const string & s) : h(s.size()), p(s.size())
 	{
 		int N = s.size();
 		h[0] = s[0];
@@ -27,58 +28,49 @@ struct Hash
 
 	int value(int idx, int len)
 	{
-		if (idx == 0)
+		if (idx == 0) {
 			return h[len - 1];
+		}
 		return (h[idx + len - 1] - 1LL * h[idx - 1] * p[len] % MOD + MOD) % MOD;
 	}
-
 };
 
 int main()
 {
     ios_base::sync_with_stdio(false); cin.tie(nullptr);
-    int N;
-    string s;
-    cin >> N >> s;
+	int N, M;
+	cin >> N >> M;
 
-    Hash hash(s);
-    vector<pair<int, int>> oc;
-
-    for (int i=1; i < N; i++) {
-        int lo = i + 1, hi = min(i * 2, N);
-        bool found = false;
-        while (lo <= hi) {
-            int mid = (lo + hi) / 2;
-            int len = mid - i;
-            if (hash.value(0, len) == hash.value(i, len)) {
-                lo = mid + 1;
-                found = true;
-            } else {
-                hi = mid - 1;
-            }
-        }
-        if (found) {
-            oc.emplace_back(i, i);
-            oc.emplace_back(lo - 1, -i);
-        }
-    }
-
-    sort(oc.begin(), oc.end());
-
-    ll ans = 0;
-	int lo=0;
-	set<int> candidates;
+	vector<Hash> h;
+	vector<Hash> rh;
 	for (int i=0; i < N; i++) {
-		while (lo < oc.size()) {
-			auto [j, n] = oc[lo];
-			if (j > i) break;
-			if (n < 0) candidates.erase(-n);
-			else candidates.insert(n);
-			lo++;
-		}
-		if (!candidates.empty()) ans += *prev(candidates.end());
+		string s;
+		cin >> s;
+		h.emplace_back(s);
+		reverse(s.begin(), s.end());
+		rh.emplace_back(s);
 	}
-    cout << ans << endl;
+
+	for (int k=min(N, M); k >= 2; k--) {
+		for (int i=0; i <= N - k; i++) {
+			for (int j=k; j <= M; j++) {
+				bool find = true;
+				for (int l=0; l <= k / 2; l++) {
+					int ol = k - l - 1;
+					if (h[i + l].value(j - k, k) != rh[i + ol].value(M - j, k)) {
+						find = false;
+						break;
+					}
+				}
+				if (find) {
+					cout << k << endl;
+					return 0;
+				}
+			}
+		}
+
+	}
+
+	cout << -1 << endl;
     return 0;
 }
-
